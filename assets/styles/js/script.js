f3788c406eed6909c7d15a47f95b58b70c68f000
BlankCanvas1006 <105467929+BlankCanvas1006@users.noreply.github.com>
2022-06-18 12:21:48 -0400

    asset commits

---
+++ assets/styles/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("name").onclick = function(){
+	var name = prompt("Hello! Thanx for visiting my page! I hope that my teachers like my site. If you wouldn't mind may I have your name : ");
+  document.getElementById("outputName").innerText = name;
+}
